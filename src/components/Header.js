5ea70e5864bff5bfeb00dbe7b158b352f9343213
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-08-31 11:13:16 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,32 @@ import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import { ShoppingBasket } from "@mui/icons-material";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
-import { useState } from "react";
 const Header = () => {
   const focusRef = useRef(0);
+  const falltenArray = (array, depth = 1) => {
+    let result = [];
+    array.forEach((arr) => {
+      if (Array.isArray(arr) && depth > 0) {
+        result.push(...falltenArray(arr, depth - 1));
+      } else result.push(arr);
+    });
+    return result;
+  };
 
+  function fun() {
+    for (var a = 0; a < 3; a++) {
+      setTimeout(function log() {
+        // let v = fun();
+        // console.log(v);
+        console.log(a);
+        // return log();
+      });
+    }
+  }
+  fun();
+  const ar = [1, 2, 3, [1, 2, 3, [4, 5, 6]], 3];
+
+  console.log(ar.flat(1));
   const onClick = () => {
     focusRef.current.focus();
   };
