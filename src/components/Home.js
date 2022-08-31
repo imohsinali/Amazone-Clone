2927b775bc4d5047ef0ac3c332fa5d5a808647fe
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-08-31 11:13:28 +0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,59 @@
 import React from "react";
-
+import "./Home.css";
+import Product from "./Product";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="home-container">
+        <img
+          className="home-image"
+          src="https://m.media-amazon.com/images/I/717OO5QwJnL._SX3000_.jpg"
+          alt=""
+        />
+
+        <div className="home-row">
+          <Product
+            title={"The lean startuo"}
+            img={
+              "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2022/BTS/Fuji_dash_BTS_backpack_1X._SY304_CB630082004_.jpg"
+            }
+            price={19.99}
+            rating={4}
+          />
+          <Product
+            img={
+              "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/September/DashboardCards/Fuji_Dash_Fitness_1X._SY304_CB639748186_.jpg"
+            }
+            price={200}
+            rating={5}
+            title={"For your Fitness Needs"}
+          />
+        </div>
+        <div className="home-row">
+          <Product
+            img={
+              "https://m.media-amazon.com/images/I/517S7lXb+mL._AC_UF226,226_FMjpg_.jpg"
+            }
+          />
+          <Product
+            img={
+              "https://m.media-amazon.com/images/I/517S7lXb+mL._AC_UF226,226_FMjpg_.jpg"
+            }
+          />
+          <Product
+            img={
+              "https://m.media-amazon.com/images/I/41XXznOD-JL._AC_UF226,226_FMjpg_.jpg"
+            }
+          />
+        </div>
+        <div className="home-row"></div>
+        <Product
+          img={"https://m.media-amazon.com/images/I/71GOki8Sk9L._AC_SY200_.jpg"}
+          title={"Top Sellers in Baby Products for you"}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
