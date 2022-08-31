89a0b2a5bb9b633aaac6fce6d17f8946fd1f98d1
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-08-31 11:13:08 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import Home from "./components/Home";
 function App() {
   return (
     <div className="App">
       {/* Header */}
       <Header />
       {/* Home */}
+      <Home />
     </div>
   );
   // Header
