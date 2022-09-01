d7359e3e10c37a14f15ad96377ab8818fbbff127
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-09-01 10:28:45 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import Header from "./components/Header";
 import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Checkout from "./components/Checkout";
 function App() {
   return (
-    <div className="App">
-      {/* Header */}
-      <Header />
-      {/* Home */}
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Home />
+              </>
+            }
+          />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </div>
+    </Router>
   );
   // Header
   // Home
