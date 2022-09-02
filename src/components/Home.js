de9465585ca2d191049490d2889c14a2c965f18e
imohsinali <mohsinbalty@gmail.com>
2022-09-02 10:59:53 +0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,14 +13,16 @@ const Home = () => {
 
         <div className="home-row">
           <Product
+            id={1}
             title={"The lean startuo"}
             img={
               "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2022/BTS/Fuji_dash_BTS_backpack_1X._SY304_CB630082004_.jpg"
             }
-            price={19.99}
+            price={200}
             rating={4}
           />
           <Product
+            id={2}
             img={
               "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2021/September/DashboardCards/Fuji_Dash_Fitness_1X._SY304_CB639748186_.jpg"
             }
@@ -31,16 +33,22 @@ const Home = () => {
         </div>
         <div className="home-row">
           <Product
+            price={200}
+            id={3}
             img={
               "https://m.media-amazon.com/images/I/517S7lXb+mL._AC_UF226,226_FMjpg_.jpg"
             }
           />
           <Product
+            price={200}
+            id={4}
             img={
               "https://m.media-amazon.com/images/I/517S7lXb+mL._AC_UF226,226_FMjpg_.jpg"
             }
           />
           <Product
+            price={200}
+            id={5}
             img={
               "https://m.media-amazon.com/images/I/41XXznOD-JL._AC_UF226,226_FMjpg_.jpg"
             }
@@ -48,6 +56,8 @@ const Home = () => {
         </div>
         <div className="home-row"></div>
         <Product
+          price={100}
+          id={6}
           img={"https://m.media-amazon.com/images/I/71GOki8Sk9L._AC_SY200_.jpg"}
           title={"Top Sellers in Baby Products for you"}
         />
