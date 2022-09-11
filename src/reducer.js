7cab5d3790fca4eb739ebe72807fb053c924a4b4
imohsinali <mohsinbalty@gmail.com>
2022-09-11 11:18:01 +0500

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,9 @@
 export const initialState = {
   basket: [],
 };
-
+export const getBasketTotal = (basket) => {
+  basket?.reduce((amount, item) => item.price + amount, 0);
+};
 const reducer = (state, action) => {
   // console.log(action);
   switch (action.type) {
@@ -10,6 +12,24 @@ const reducer = (state, action) => {
         ...state,
         basket: [...state.basket, action.item],
       };
+    case "REMOVE_FROM_CART":
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+      let newBasket = [...state.basket];
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Cant remove product id ${action.id} as it is not in basket`
+        );
+      }
+      return {
+        ...state,
+        basket: newBasket,
+      };
+    default:
+      return state;
   }
 };
 
