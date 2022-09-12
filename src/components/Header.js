2662dd962fd7f560c7437b8b45e496c953ab027c
imohsinali <mohsinbalty@gmail.com>
2022-09-12 10:39:14 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,30 +8,6 @@ import { useStateValue } from "../StateProvider";
 const Header = () => {
   const [{ basket }, dispatch] = useStateValue();
   const focusRef = useRef(0);
-  const falltenArray = (array, depth = 1) => {
-    let result = [];
-    array.forEach((arr) => {
-      if (Array.isArray(arr) && depth > 0) {
-        result.push(...falltenArray(arr, depth - 1));
-      } else result.push(arr);
-    });
-    return result;
-  };
-
-  function fun() {
-    for (var a = 0; a < 3; a++) {
-      setTimeout(function log() {
-        // let v = fun();
-        // console.log(v);
-        // console.log(a);
-        // return log();
-      });
-    }
-  }
-  fun();
-  const ar = [1, 2, 3, [1, 2, 3, [4, 5, 6]], 3];
-
-  console.log(ar.flat(1));
   const onClick = () => {
     focusRef.current.focus();
   };
@@ -62,8 +38,10 @@ const Header = () => {
       </div>
       <div className="header-nav">
         <div className="header-option">
-          <span className="header-option-lineOne">Hello Guest</span>
-          <span className="header-option-lineTwo">Sign In</span>
+          <Link to={"login"} style={{ textDecoration: "none", color: "white" }}>
+            <span className="header-option-lineOne">Hello Guest</span>{" "}
+            <span className="header-option-lineTwo">Sign In</span>
+          </Link>{" "}
         </div>
         <div className="header-option">
           <span className="header-option-lineOne">Returns</span>
